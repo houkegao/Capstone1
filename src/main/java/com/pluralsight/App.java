4920904036784f767fef38881884c1c1f4be45dd
Houke Gao <gaohouke@gmail.com>
2024-04-29 21:25:19 -0700

    added printlns

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -1,7 +1,23 @@
 package com.pluralsight;
+import java.io.*;
+import java.util.*;
 
 public class App {
+    private static final String CSV_FILE = "transactions.csv";
+    private static final String CSV_HEADER = "date|time|description|vendor|amount";
     public static void main(String[] args) {
+        System.out.println("hello");
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
         
+        while (running) {
+            System.out.println("=== Home Screen ===");
+            System.out.println("D) Add Deposit");
+            System.out.println("P) Make Payment (Debit)");
+            System.out.println("L) Ledger");
+            System.out.println("X) Exit");
+            System.out.print("Choose an option: ");
+            running = false;
+        }
     }
 }
